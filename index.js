8aba62bf8cc040ec2a36548c8de93dd4c505a449
silvodesigns <kevsilvo@gmail.com>
2020-01-25 17:58:59 -0500

    added get id and post routes

--- index.js
+++ index.js
@@ -15,11 +15,16 @@ Go code!
 
 const express = require('express');
 
+const projectsRoutes = require('./resources/projects.js');
+
 const server = express();
 
 server.get('/', (req, res) => {
     res.send("Hello from Express Sprint");
 })
+
+
+server.use('/projects', projectsRoutes);
 
 server.listen(5000, () =>{
     console.log("API is now running on port 5000");
--- resources/projects.js
+++ resources/projects.js
@@ -17,5 +17,36 @@ router.get('/', (req, res) => {
      })
 })
 
+router.get('/:id', (req, res) => {
+    project_db.get(req.params.id)
+    .then(project => {
+        res.status(200);
+        res.json(project);
+    })
+    .catch(()=> {
+        res.status(404);
+        res.json({"message": "the project you were trying to find does not exist"})
+    })
+});
+
+router.post('/', (req, res) => {
+    const {name, description } = req.body;
+
+    if(!name || !description){
+        res.status(400).json({errorMessage: "Please provide a name and description for the project"})
+    } else {
+
+                project_db.insert(req.body)
+                .then(project => {
+                    res.status(201);
+                    res.json(project);
+                })
+                .catch(()=> {
+                    res.status(500);
+                    res.json({"message": "Could post the project error"})
+                })
+            }       
+});
+
 
 module.exports = router;
