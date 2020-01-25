8f6c022dc4a54fce264da24c93bbf13f4ddd9252
silvodesigns <kevsilvo@gmail.com>
2020-01-25 18:50:13 -0500

    added delete put and post

--- resources/actions.js
+++ resources/actions.js
@@ -31,5 +31,70 @@ router.get('/:id', (req, res) => {
     })
 });
 
+router.post('/', (req, res) => {
+
+    const { notes, description } = req.body;
+
+    if(!description || !notes){
+        res.status(400).json({errorMessage: "Please provide  a description , note and id for the action"})
+    } else {
+                project_db.get(req.params.id)
+                .then(project => {
+                    if(project){
+                            action_db.insert({...req.body, project_id : req.params.id})
+                            .then(action => {
+                                res.status(201);
+                                res.json(action);
+                            })
+                            .catch(()=> {
+                                res.status(500);
+                                res.json({"message": "Could not post the action error"})
+                            })
+                        } else {
+
+                            res.status(500);
+                            res.json({"message": "The project with this ID does not exist so action could not be created"})
+
+                        }
+                })
+            
+               
+            }       
+});
+
+
+router.put('/:id', (req, res) => {
+
+    const {project_id, notes, description } = req.body;
+
+    if(!project_id ||!description || !notes){
+        res.status(400).json({errorMessage: "Please provide a name and description for the action to update"})
+    } else {
+
+                action_db.update(req.params.id,{...req.body})
+                .then(action => {
+                    res.status(201);
+                    res.json(action);
+                })
+                .catch(()=> {
+                    res.status(500);
+                    res.json({"message": "Could not update the action for the project"})
+                })
+            }       
+});
+
+router.delete('/:id', (req, res) => {
+   action_db.remove(req.params.id)
+    .then(action => {
+        res.status(200);
+        res.json({"message": "The action with specified ID was successfully deleted"})
+    })
+    .catch( () =>{
+        res.status(500);
+        res.json({"errorMessage": "The action with specified ID could not be deleted"})
+    })
+})
+
+
 
 module.exports = router
