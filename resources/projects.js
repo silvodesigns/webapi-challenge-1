43d435f8e1528eb2e15a16e32a3f4ef0d0d29c5e
silvodesigns <kevsilvo@gmail.com>
2020-01-25 17:50:27 -0500

    adding project model helper

--- resources/projects.js
+++ resources/projects.js
@@ -1,11 +1,20 @@
 const express = require('express');
 
+const project_db= require('../data/helpers/projectModel');
 const router = express.Router();
 router.use(express.json());
 
 
 router.get('/', (req, res) => {
-    res.send("welcome to projects")
+    project_db.get()
+     .then(projects => {
+         res.status(200);
+         res.json(projects);
+     })
+     .catch(()=> {
+         res.status(404);
+         res.json({"message": "the projects you were trying to find do not exist"})
+     })
 })
 
 
