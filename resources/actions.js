428e205415bf4631fc3e0bb5778cab02c47c712a
silvodesigns <kevsilvo@gmail.com>
2020-01-25 18:15:36 -0500

    added get on action file

---
+++ resources/actions.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const action_db = require('../data/helpers/actionModel');
+const project_db = require('../data/helpers/projectModel');
+
+//middleware that parses request body if it is json when making post requests
+router.use(express.json());
+
+
+router.get('/', (req, res) => {
+    project_db.getProjectActions(req.params.id)
+    .then(actions => {
+        res.status(200);
+        res.json(actions);
+    })
+    .catch(()=> {
+        res.status(404);
+        res.json({"message": "the actions from the specified project does not exist"})
+    })
+});
+
+
+module.exports = router
